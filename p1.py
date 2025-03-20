1d1c9963ee79916f755a0ef939a39f51c2900554
rajdeepti0402@gmail.com <rajdeepti0402@gmail.com>
2025-03-20 11:12:01 +0530

    p1.py was added

---
+++ p1.py
@@ -0,0 +1,12 @@
+print(1)
+print(1+3)
+print("My name is"+"Deepti")
+print("My name is {}")
+#input("Enter your name:")
+a=19
+print(f"the values stored is {a}")        #f for formatted string
+a,b,c=10,20,30
+print(a)
+print(b)
+print(c)
+#data type: int, float, boolean ,string ,lists, tuple, dictionary, sets
